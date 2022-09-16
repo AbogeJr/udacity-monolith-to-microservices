21bfe89cb97be6b40b2fa0200671b61b6e5c6e2b
AbogeJr <junioraboge@gmail.com>
2022-09-16 09:18:14 +0300

    fixes

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: "Udagram",
   apiHost:
-    "http://a7f3d60cfbe8743b9b67e1ba45667591-741696827.us-east-2.elb.amazonaws.com:8080/api/v0",
+    "http://localhost:8080/api/v0",
 };
 
 /*
